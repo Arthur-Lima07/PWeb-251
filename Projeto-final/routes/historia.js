71ef07cb36392e1d527d501732e4fc73a4f92ebd
Arthur-Lima07 <arthur13pe@gmail.com>
2025-07-03 00:13:16 -0300

    Testando commit via VS Code em 03/07/2025

--- Projeto-final/routes/historia.js
+++ Projeto-final/routes/historia.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
@@ -5,4 +6,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('historia');
 });
+=======
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('historia');
+});
+>>>>>>> 21f072c (Testando commit via VS Code em 03/07/2025)
 module.exports = router;
