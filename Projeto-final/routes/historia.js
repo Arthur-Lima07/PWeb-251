f8ae50e2fc7adf47f31c917fe8f73455e9726b33
Arthur-Lima07 <arthur13pe@gmail.com>
2025-07-08 11:17:12 -0300

    Atualizando imagem de fundo

--- Projeto-final/routes/historia.js
+++ Projeto-final/routes/historia.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
@@ -6,13 +5,4 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('historia');
 });
-=======
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('historia');
-});
->>>>>>> 21f072c (Testando commit via VS Code em 03/07/2025)
 module.exports = router;
